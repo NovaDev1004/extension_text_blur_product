3c590017aad40a85f2dbdc2a6484d8d8b14cddb6
novadev1004 <novadev_group@outlook.com>
2024-10-31 16:35:19 +1100

    fixed chrome extension
    This extension can't be control using UI

---
+++ README.md
@@ -0,0 +1,215 @@
+# Chrome Extension for blurring text 
+This extension can blur specified text/keywords in web pages.
+
+# Demo
+## Keyword matching
+
+https://github.com/horihiro/TextBlurrer-ChromeExtension/assets/4566555/f33e548c-c360-42fd-9777-dbf89187588b
+
+## Regular Expression matching
+https://github.com/horihiro/TextBlurrer-ChromeExtension/assets/4566555/dbd7d6f7-3879-4ab0-895c-8c6b154fecf2
+
+#### RegExp Validation
+https://github.com/horihiro/TextBlurrer-ChromeExtension/assets/4566555/f7c86192-a407-4497-b4e6-ff1c98f461ca
+
+## Show value on mouseover (v0.1.3 or later)
+https://github.com/horihiro/TextBlurrer-ChromeExtension/assets/4566555/6f3753dd-9031-43df-87af-f9f572f7b758
+
+## Blur value in `input` tag (experimental, v0.1.3 or later)
+https://github.com/horihiro/TextBlurrer-ChromeExtension/assets/4566555/27f2c3b1-ecf0-4a1f-88df-2f6e2b948f06
+
+## Exclusion URL list (v0.2.0 or later)
+If you want NOT to blur keywords in specific sites/frames, you can specify URLs of the site in the `Exclusion URL patterns` tab.
+
+![image](https://github.com/horihiro/TextBlurrer-ChromeExtension/assets/4566555/7b38cd45-66dd-4021-96a8-e67180e8cc4f)
+
+You can add URLs on the current tab to the list by clicking `+ Add URLs in the current tab` on the popup. 
+
+## Context menu for adding keyword (v0.2.0 or later)
+
+Simple way to add the blurry keyword.
+
+![image](https://github.com/horihiro/TextBlurrer-ChromeExtension/assets/4566555/a1182e7f-5462-493b-b561-618863d29fc9)
+
+## Shortcut keys on popup (v0.2.0 or later)
+
+  - <kbd>Ctrl</kbd> / <kbd>⌘</kbd> + <kbd>s</kbd>: applying keywords/url patterns change (i.e. pressing `Apply` button)
+  - <kbd>Shift</kbd> + <kbd>Alt</kbd> + <kbd>f</kbd>: removing empty lines in active textarea
+
+## Mask value in title (experimental, v0.1.9 or later)
+https://github.com/horihiro/TextBlurrer-ChromeExtension/assets/4566555/25c24d50-b478-4e63-9388-20b391046fc6
+
+
+# Try this
+This extension can be installed from [Chrome Web Store](https://chrome.google.com/webstore/detail/text-blurrer/mbikojdgkmpjfackcmiliemgmkdkbbcl).
+
+If you can try a development version, the following steps are needed.
+
+1. get contents of this repository
+    1. clone this repository  
+      or
+    1. download zip file and extract it
+1. open `chrome://extensions`
+1. enable `Developer mode` and click `Load Unpacked`
+    1. Google Chrome  
+      ![image](https://github.com/horihiro/TextBlurrer-ChromeExtension/assets/4566555/0656fd3d-41da-4f97-a614-da232a3d700d)
+    1. Microsoft Edge  
+      ![image](https://github.com/horihiro/TextBlurrer-ChromeExtension/assets/4566555/44e7f896-9e82-4af1-ae1b-f864097b44c7)
+1. select the directory created by cloning at step 1.
+
+# Limitations
+  - These are **experimental** features
+    - Input Element blurring
+    - Tab title masking
+  - The following HTML elements are not supported:
+    - HTML Canvas
+    - TextArea
+  - Web Terminal based on Xterm.js:  
+    see [here](./docs/BLUR_ON_XTERMJS.md)
+  - Web Editor based on CodeMirror:  
+    CodeMirror, which bases GitHub code editor, repairs its own contents automatically after blurring by this extension. Then blurring and repairing are repeated, it causes infinity loop. ([#62](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/62))
+
+    > [!NOTE]
+    > Not limited to CodeMirror, it's possible that an infinity loop and a site freezing happen caused by repeat of blurring and DOM tree keeping when the extension tries to blur text on a site composited by frameworks or components which try to keep DOM tree. 
+    > As long as I investigate, lexical is also such a component.
+
+# Dependencies
+  - **[jsdiff](https://github.com/kpdecker/jsdiff)**: A JavaScript text differencing implementation (BSD 3-Clause License).
+
+# Change logs
+
+## [0.2.5](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.2.5)
+
+  - Bug fixes
+    - Improve performance if there are **invisible** (`display: none` or `visibility: hidden`) elements containing target keywords.
+  - Chores
+    - Typescriptize
+
+## [0.2.4](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.2.4)
+
+  - New features
+    - Support `contentEditable`-enabled elements
+  - Bug fixes
+    - Fix tab title masking ([#65](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/65))
+    - Fix issue with matching empty string ([#67](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/67))
+  - Chores
+    - Add documentation about priority of the keywords/patterns
+
+## [0.2.3](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.2.3)
+
+  - Bug fixes
+    - Fix tab title masking ([#56](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/56))
+    - Fix white-space handling ([#57](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/57))
+    - Fix hanging up on sites contain CodeMirror ([#62](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/62))
+  - Chores
+    - Add documentation about blurring on Xterm.js
+
+## [0.2.2](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.2.2)
+
+  - Bug fixes
+    - Improve performance ([#52](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/52))
+    - Fix adding exclusion URL patterns list
+
+## [0.2.1](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.2.1)
+
+  - Bug fixes
+    - Improve performance ([#48](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/48))
+
+## [0.2.0](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.2.0)
+
+Refactoring blurring logic to improve performance and maintainability.  
+From this version, this extension includes [jsdiff](https://github.com/kpdecker/jsdiff)
+
+  - New features
+    - Disable blurring on listed sites on [Exclusion URL pattern list](#exclusion-url-list-v020-or-later)
+    - Add [Context Menu for adding the blurry keywords](#context-menu-for-adding-keyword-v020-or-later)
+    - Add shortcut keys on popup :
+      - <kbd>Ctrl</kbd> / <kbd>⌘</kbd> + <kbd>s</kbd>: applying keywords/url patterns change
+      - <kbd>Shift</kbd> + <kbd>Alt</kbd> + <kbd>f</kbd>: removing empty lines in active textarea
+  - Bug fixes
+    - Improve title masking ([#38](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/38), [#39](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/39))
+    - Improve unblurring on updating text node by javascript ([#40](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/40))
+    - Improve inputting keywords using RegExp assersions ([#41](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/31))
+
+## [0.1.9](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.1.9)
+
+  - New features
+    - Add **experimental** option to mask title by keywords
+  - Bug fixes
+    - Improve misalignment of mask position for input element
+
+## [0.1.8](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.1.8)
+
+  - Bug fixes
+    - Fix misalignment of mask position for input element with `box-sizing` set to `border-box` ([#32](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/32))
+
+## [0.1.7](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.1.7)
+
+  - Bug fixes
+    - Fix handling empty line in keyword/pattern list([#29](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/29))
+
+## [0.1.5](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.1.5)
+
+  - Bug fixes
+    - Fix misalignment of mask position([#26](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/26))
+    - Improve performance with many keywords/patterns
+
+## [0.1.4](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.1.4)
+
+  - New features
+    - Add a link to [new issue](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/new)
+  - Bug fixes
+    - Fix logic for getting backgroud color
+    - Restore `title` attribute
+
+## [0.1.3](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.1.3)
+
+  - New features
+    - Blur keywords splitted into some elements ([#2](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/2))
+    - Show blurred value on mouse over blurred area 
+    - Blur value in `input` tag (experimental)  
+      :warning:  this cannot detect change by `value` property and javascript
+  - Bug fixes
+    - Improve performance by change blurring logic
+    - Blur keywords in shadow DOM
+
+## [0.1.2](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.1.2)
+
+  - New features
+    - Add warning on popup for RegExp one character matches
+  - Bug fixes
+    - Improve performance
+
+## [0.1.1](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.1.1)
+
+  - Bug fixes
+    - Make RegExp/Matching Case buttons more clear
+
+## [0.1.0](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.1.0)
+
+  -  New features
+    - New Popup UI
+      - Add buttons for:
+        - Regular Expression ([#3](https://github.com/horihiro/TextBlurrer-ChromeExtension/issues/3)) w/ validation
+        - Match case
+
+  - Bug fixes
+    - `Enabled` toggle button behavior
+
+## [0.0.2-alpha](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.0.2)
+
+First release on [Chrome Web Store](https://chrome.google.com/webstore/detail/text-blurrer/mbikojdgkmpjfackcmiliemgmkdkbbcl).
+
+  - Bug fixes
+    - Improve performance
+
+  - Chores
+    - Remove unnesessary features
+      - server worker
+      - `tabs` permission
+      - dependency to jQuery
+
+## [0.0.1-alpha](https://github.com/horihiro/TextBlurrer-ChromeExtension/releases/tag/0.0.1)
+
+First release on GitHub
+
---
+++ dist/popup/js/bundle.js
@@ -0,0 +1,203 @@
+(() => {
+  function W(V) {
+      return V.replace(/([\(\)\{\}\+\*\?\[\]\.\^\$\|\\])/g, "\\$1")
+  }
+  document.addEventListener("DOMContentLoaded", async V => {
+      let {
+          status: D,
+          keywords: _,
+          mode: N,
+          matchCase: j,
+          showValue: G,
+          blurInput: Y,
+          blurTitle: z,
+          exclusionUrls: J
+      } = await chrome.storage.local.get(["status", "keywords", "mode", "matchCase", "showValue", "blurInput", "blurTitle", "exclusionUrls"]), h = document.querySelector("#applyButton"), t = document.querySelector("#patternInput"), p = document.querySelector("#statusCheckbox"), f = document.querySelector("#caseCheckbox"), u = document.querySelector("#regexpCheckbox"), m = document.querySelector("#showValueCheckbox"), y = document.querySelector("#blurInputCheckbox"), b = document.querySelector("#blurTitleCheckbox"), g = document.querySelector("#_bufferTextArea"), w = document.querySelector("#addUrlsInCurrentTab"), r = document.querySelector("#exclusionTextArea");
+      if (!h || !t || !p || !f || !u || !m || !y || !b || !g || !w || !r) throw new Error("Element not found");
+      let x = getComputedStyle(g).getPropertyValue("background-color"),
+          Q = "#FFA500",
+          X = "#FF4500",
+          H = 10,
+          L = getComputedStyle(t),
+          Z = parseInt(L.getPropertyValue("padding-left")),
+          E = parseInt(L.getPropertyValue("padding-top")),
+          ee = parseInt(L.getPropertyValue("line-height")),
+          T = "",
+          v = "",
+          C = !1,
+          S = !1,
+          $ = !1,
+          I = !1,
+          M = !1,
+          k = {};
+      window.addEventListener("keydown", e => {
+          if (e.key === "s" && (e.ctrlKey && !e.metaKey || !e.ctrlKey && e.metaKey)) {
+              e.preventDefault(), h.click();
+              return
+          }
+          if (e.key === "F" && e.altKey && e.shiftKey) {
+              let a = document.querySelector("#tab-exclusion:checked~#tab-panel-exclusion textarea") || document.querySelector("#tab-keywords:checked~#tab-panel-keywords textarea");
+              if (!a) {
+                  e.preventDefault();
+                  return
+              }
+              a.focus(), a.value = a.value.split(/\n/).filter(l => l.trim() !== "").join(`
+`), e.preventDefault();
+              return
+          }
+      }), chrome.runtime.onMessage.addListener((e, a, l) => {
+          e.method === "reload" && window.location.reload()
+      });
+      let te = async (e, a, l) => {
+          if (e.method !== "getUrlResponse") return;
+          let s = W(e.url),
+              d = r.value.split(/\n/);
+          d.includes(s) || d.filter(n => !!n).some(n => new RegExp(n).test(e.url)) || (r.value = `${r.value.trimEnd()}
+^${s}$`, await o({
+              target: r
+          }), h.disabled = !1, r.focus())
+      };
+      chrome.runtime.onMessage.addListener(te), w.addEventListener("click", async e => {
+          if (!p.checked) return;
+          let l = (await chrome.tabs.query({
+              active: !0,
+              currentWindow: !0
+          }))?.[0]?.id;
+          l && await chrome.tabs.sendMessage(l, {
+              method: "getUrl"
+          })
+      });
+      let ae = e => new Promise(a => {
+              e === "" && a(!1);
+              try {
+                  let l = new RegExp(e).source.replace(/\\.|\[[^\]]*\]/g, "x");
+                  a(/\((?:[^?]|(?=\?<[^!>]+>))/.test(l))
+              } catch {
+                  a(!0)
+              }
+          }),
+          ne = e => new Promise(a => {
+              e === "" && a(!0);
+              try {
+                  new RegExp(e), a(!0)
+              } catch {
+                  a(!1)
+              }
+          }),
+          re = (e, a) => new Promise(l => {
+              let s = window.getComputedStyle(e),
+                  d = parseInt(s.paddingLeft),
+                  n = parseInt(s.paddingRight),
+                  i = parseInt(s.lineHeight);
+              isNaN(i) && (i = parseInt(s.fontSize)), g.style.width = e.clientWidth - d - n + "px", g.style.font = s.font, g.style.letterSpacing = s.letterSpacing, g.style.whiteSpace = s.whiteSpace, g.style.wordBreak = s.wordBreak, g.style.wordSpacing = s.wordSpacing, g.style.wordWrap = s.wordWrap, g.value = a;
+              let c = Math.floor(g.scrollHeight / i);
+              l(c == 0 ? 1 : c)
+          }),
+          R = async (e, a) => await e.value.split(/\n/).reduce(async (s, d) => {
+              let n = await re(e, d),
+                  i = await s,
+                  c = {
+                      numOfLine: n,
+                      isValid: !0,
+                      reason: ""
+                  };
+              return a || (await ne(d) ? await ae(d) ? (c.isValid = !1, c.reason = "Error:\n  This string might contain capture-group that should be non-capture-group.\n  Replace a pair of `(` and `)` to `(?:` and `)`.") : /^(?:\.|(?:\\[^\\])|(?:\[[^\]]+\]))(?:\?|\*|\+|\{,?1\}|\{1,(?:\d+)?\})?$/.test(d) ? c.reason = `Warning:
+                                                                                    One character matching might cause performance issue.` : d !== "" && new RegExp(d).test("") && (c.isValid = !1, c.reason = `Error:
+                                                                                    This pattern matches an empty string.`) : (c.isValid = !1, c.reason = `Error:
+                                                                                    Failed to create RegExp object.
+                                                                                    Check if this is a valid regular expression string.`)), i.push(c), i
+            }, []), o = async e => {
+              if (e.target.value.split(/\n/).length == 0) {
+                  h.disabled = !1;
+                  return
+              }
+              u.checked && (h.disabled = !1), k[e.target.id] = await R(e.target, !u.checked), h.disabled = !k[e.target.id].every(n => n.isValid) || t.value === T && r.value === v && f.checked === C && m.checked === $ && u.checked === S && y.checked === I && b.checked === M;
+              let l = /\*(\d+)( - [\d.]+px\))$/,
+                  s = k[e.target.id].reduce((n, i, c, F) => {
+                      let K = i.isValid ? i.reason ? Q : x : X;
+                      if (c == 0) return n.push(`${K} calc(var(--l)*0 - ${e.target.scrollTop}px) calc(var(--l)*${i.numOfLine} - ${e.target.scrollTop}px)`), n;
+                      let A = parseInt(n[n.length - 1].match(l)[1]);
+                      return i.isValid == F[c - 1].isValid && !!i.reason == !!F[c - 1].reason ? (n[n.length - 1] = n[n.length - 1].replace(l, `*${A+i.numOfLine}$2`), n) : (n.push(`${K} calc(var(--l)*${A} - ${e.target.scrollTop}px) calc(var(--l)*${A+i.numOfLine} - ${e.target.scrollTop}px)`), n)
+                  }, []);
+              if (s.length > 0) {
+                  let n = parseInt(s[s.length - 1].match(l)[1]);
+                  s.push(`${x} calc(var(--l)*${n} - ${e.target.scrollTop}px) calc(var(--l)*${n+1} - ${e.target.scrollTop}px)`), e.target.setAttribute("rows", n > H ? n : H)
+              }
+              let d = document.querySelector(`head > style#style-${e.target.id}`);
+              d && (d.innerHTML = `textarea#${e.target.id} {
+                                    background: linear-gradient(
+                                    ${s.join(`,
+                                    `)}
+                                    ) 0 8px no-repeat, ${x};
+                                    }`)
+          };
+      h.addEventListener("click", async e => {
+          await chrome.storage.local.set({
+              status: "",
+              keywords: "hello how keyword",
+              exclusionUrls: r.value,
+              mode: "text",
+              matchCase: true,
+              showValue: true,
+              blurInput: true,
+              blurTitle: true
+          }), t.focus(), T = t.value, v = r.value, S = u.checked, C = f.checked, $ = m.checked, I = y.checked, M = b.checked, e.target && e.target instanceof HTMLButtonElement && (e.target.disabled = !0)
+      }), p.addEventListener("change", async e => {
+          if (!(!e.target || !(e.target instanceof HTMLInputElement))) {
+              if (f.disabled = m.disabled = y.disabled = b.disabled = u.disabled = t.disabled = r.disabled = !e.target.checked, k[t.id] = await R(t, !u.checked), k[r.id] = await R(r, !u.checked), w.style.cursor = e.target.checked ? "" : "auto", h.disabled = !e.target.checked || !k[t.id].every(a => a.isValid) || !k[r.id].every(a => a.isValid), await chrome.storage.local.set({
+                      status: e.target.checked ? "" : "disabled"
+                  }), !e.target.checked) {
+                  let a = document.querySelector(`head > style#style-${t.id}`);
+                  a && (a.innerHTML = "");
+                  let l = document.querySelector(`head > style#style-${r.id}`);
+                  l && (l.innerHTML = "");
+                  return
+              }
+              await o({
+                  target: t
+              }), await o({
+                  target: r
+              }), t.focus()
+          }
+      }), u.addEventListener("change", async e => {
+          t.focus(), t.style.background = x, t.style.background = "", await o({
+              target: t
+          })
+      }), f.addEventListener("change", async e => {
+          await o({
+              target: t
+          })
+      }), m.addEventListener("change", async e => {
+          await o({
+              target: t
+          }), t.focus()
+      }), y.addEventListener("change", async e => {
+          await o({
+              target: t
+          }), t.focus()
+      }), b.addEventListener("change", async e => {
+          await o({
+              target: t
+          }), t.focus()
+      });
+      let q = e => {
+              e.target.scrollLeft < Z && (e.target.scrollLeft = 0), e.target.scrollTop < E && (e.target.scrollTop = 0)
+          },
+          O = e => {
+              if (e.offsetY + e.target.scrollTop - E < 0) return;
+              let a = parseInt(`${(e.offsetY+e.target.scrollTop-E)/ee}`) + 1;
+              e.target.pointedRow != a && (e.target.pointedRow = a, k[e.target.id]?.reduce((l, s) => l < 0 ? -1 : (l -= s.numOfLine, l > 0 ? (e.target.title = "", l) : (e.target.setAttribute("title", s.reason || ""), -1)), a))
+          },
+          U = e => {
+              e.target.pointedRow = -1
+          },
+          B = document.createElement("style");
+      document.head.appendChild(B), B.setAttribute("id", `style-${t.id}`), t.addEventListener("scroll", o), t.addEventListener("scroll", q), t.addEventListener("input", o), t.addEventListener("mousemove", O, !1), t.addEventListener("mouseout", U);
+      let P = document.createElement("style");
+      document.head.appendChild(P), P.setAttribute("id", `style-${r.id}`), r.addEventListener("scroll", o), r.addEventListener("scroll", q), r.addEventListener("input", o), r.addEventListener("mousemove", O, !1), r.addEventListener("mouseout", U), p.checked = D !== "disabled", C = f.checked = j, $ = m.checked = G, I = y.checked = Y, M = b.checked = z, S = u.checked = N === "regexp", T = t.value = _ || "", v = r.value = J || "", f.disabled = y.disabled = b.disabled = m.disabled = u.disabled = t.disabled = r.disabled = h.disabled = !p.checked, w.style.cursor = p.checked ? "" : "auto", t.focus(), p.checked && (await o({
+          target: t
+      }), await o({
+          target: r
+      })), h.disabled = !0
+  });
+})();
